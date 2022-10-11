5251d95f9afc0340869c5c814343d49512fc0f97
stephany souza <stephanysr@hotmail.com>
2022-10-10 21:05:57 -0300

    camada repository

--- farmacia/src/main/java/com/farmacia/project/repository/CategoriaRepository.java
+++ farmacia/src/main/java/com/farmacia/project/repository/CategoriaRepository.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.farmacia.project.model.Categoria;
-
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
 	public List<Categoria> findAllBySetorContainingIgnoreCase(@Param("setor") String setor);
